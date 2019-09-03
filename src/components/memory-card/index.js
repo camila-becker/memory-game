d9629c60e624595a164d836ae79530435c52aeea
Camila Becker <camila.becker01@gmail.com>
2019-09-03 17:16:00 -0300

    Correção desafio aula 024

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -1,25 +1,18 @@
-const createMemoryCard = () => `
-    <article  class="memory-card">
-      <img 
-        class='icon' 
-        src='img/icon-collabcode.png' 
-        alt='Gueio Mascote da CollabCode'
-        onClick = "handleClick()"
-      />
-    </article>
-  `;
+const createMemoryCard = nameClass => `
+  <article class="memory-card ${nameClass}">
+    <img 
+      class="icon" 
+      src="${
+        nameClass == "-front" ? "img/-icon-js.png" : "img/icon-collabcode.png"
+      }" 
+      alt="${
+        nameClass == "-front"
+          ? "Íconde de um livro de JavaScript"
+          : "Gueio mascote da CollabCode"
+      }"
+      onClick = "handleClick()"
+    />
+  </article>
+`;
 
-const createMemoryCardFront = () => `
-    <article class="memory-card -front">
-      <img 
-        class='icon'
-        src='img/-icon-js.png' 
-        alt='Ícone de um livro da linguagem JavaScript' 
-        onClick="handleClick()"
-      />      
-    </article>
-  `;
-
-function handleClick() {
-  console.log("Clicado");
-}
+const handleClick = () => console.log("Clicado");
